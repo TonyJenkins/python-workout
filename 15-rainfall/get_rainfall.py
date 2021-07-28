2a3d9040958c5b895f6a9b3e7b136acca2a99f22
Tony Jenkins <tony@tony-jenkins.co.uk>
2021-07-28 16:03:38 +0100

    Add first version of Exercise 15

---
+++ 15-rainfall/get_rainfall.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+    Exercise 15: Printing Tuple Records
+
+    Gather and summarise rainfall data.
+"""
+
+
+def get_rainfall():
+
+    rainfalls = {}
+    while True:
+        city = input('Enter City: ')
+
+        if not city:
+            break
+        else:
+            try:
+                rain = int(input('Enter Rain: '))
+                rainfalls[city] = rainfalls.get(city, 0) + rain
+            except ValueError:
+                print('Non-integer entered. Data ignored.')
+
+    print()
+    for city, rain in rainfalls.items():
+        print(f'{city}: {rain}')
+
+
+if __name__ == '__main__':
+    get_rainfall()
