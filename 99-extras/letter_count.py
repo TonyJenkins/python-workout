2dae1f2ce75ebae6c6598c9d3dc9d763a84f1588
Tony Jenkins <tony@tony-jenkins.co.uk>
2021-08-09 17:27:02 +0100

    Tweak letter count to remove ugly initial dict

--- 99-extras/letter_count.py
+++ 99-extras/letter_count.py
@@ -7,25 +7,21 @@
 """
 
 
+from string import ascii_lowercase as letters
+
+
 def letter_count(filename):
 
-    counts = {
-        'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0,
-        'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0,
-        'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0,
-        'm' : 0, 'n' : 0, 'o' : 0, 'p' : 0,
-        'q' : 0, 'r' : 0, 's' : 0, 't' : 0,
-        'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0,
-        'y' : 0, 'z' : 0,
-    }
+    counts = {}
 
     with open(filename) as f:
         while 1:
-            c = f.read(1)
+            c = f.read(1).lower()
             if not c:
                 break
-            elif c.lower() in counts.keys():
-                counts[c.lower()] += 1
+            elif c in letters:
+                counts.setdefault(c, 0)
+                counts[c] += 1
 
     print(sorted(counts, key=counts.get, reverse=True)[:5])
 
